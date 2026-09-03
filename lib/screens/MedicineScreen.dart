d5a39e0fa59bbf6d0da8d89b50c5d2063bdc8c4b
PraProg777 <practicaprogramacion777@gmail.com>
2026-09-03 15:24:00 +0000

    Se crearon los archivo HomeScreen.dart, MedicineScreen.dart, CalenderScreen.dart y SettingScreen.dart. Al mismo tiempo se le puso lo minimo e indispensable para que anden.

---
+++ lib/screens/MedicineScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MedicineScreen extends StatelessWidget {
+  const MedicineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Pantalla de medicamentos (en contrsuccion)')
+    );
+  }
+}
